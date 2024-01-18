f1b062ff901871d1eceda51262f483d9514dcc35
Adnan <adnanvandyck@gmail.com>
2024-01-18 00:17:58 -0500

    Mole hill image rendering

--- src/Mole.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Mole() {
-  return (
-    <div>Mole</div>
-  )
-}
-
-export default Mole
---
+++ src/components/MoleContainer.js
@@ -0,0 +1,21 @@
+import React, { useState }from 'react'
+import Mole from './Mole'
+import EmptySlot from './EmptySlot'
+
+
+const MoleContainer = (props) => {
+  let [theMole, setTheMole] = useState(false)
+
+  const handleClick = (e) => {
+    props.setScore(props.score + 1)
+    setTheMole(false)
+  }
+  let displayMole = theMole ? <Mole setScore={props.setScore} toggle={setTheMole} handleClick={handleClick} /> : <EmptySlot />
+  return (
+    <div>
+        {displayMole}
+    </div>
+  )
+}
+
+export default MoleContainer
